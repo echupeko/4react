24c2b2a92d8529479f740ad63ba402f37d0785a8
Евгений <echupeko@gmail.com>
2018-10-24 15:25:13 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,68 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-import Button from './components/Button';
-import TodoItem from './components/TodoItem';
-
-
 
 class App extends React.Component {
 
-    state = {
-        showDescription: false,
-        todoList: []
-    };
-
-
-    deleteTodo = (index) => (event) => {
-        console.log('del event', event);
-        console.log('del index', index);
-        const list = this.state.todoList;
-        list.splice(index, 1);
-        this.setState({todoList: list})
-
-    };
-
-
-    handleClick = (event) => {
-        console.log('todolist', this.state.todoList);
-        const list = this.state.todoList;
-        list.push({title: 'todo Title', description: 'lalalalend'});
-        // this.setState({showDescription: !this.state.showDescription})
-
-        this.setState({todoList: list})
-    };
-
-    titleChangeHandle = (index) => (event) => {
-        console.log('eevent2', event);
-        const list = this.state.todoList;
-        list[index].title = event.target.value;
-        this.setState({todoList: list})
-    };
-
-
-    changeDescriptionHandle = (event) => {
-
-        const list = this.state.todoList;
-        list[event.index].description = event.value;
-        this.setState({todoList: list})
-    };
-
     render() {
-        const {showDescription, todoList} = this.state;
-        console.log('todo', todoList);
+
         return (
             <div className="App">
-                <Button handleButtonClick={this.handleClick} description={showDescription ? 'Well done' : ''}/>
-                {todoList.map((element, index) =>
-                    <TodoItem
-                        /*index={index}
-                        titleChangeHandle={this.titleChangeHandle}
-                        changeDescriptionHandle={this.changeDescriptionHandle}
-                        deleteTodo={this.deleteTodo}*/
-                        title={element.title}
-                        disabled={element.disabled}
-                        key={index}/>)}
+                <Header/>
             </div>
         );
     }

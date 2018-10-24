9c4b4ff8d4ab4e30c984fb4cb7aa0373b4579b0b
Евгений <echupeko@gmail.com>
2018-10-24 14:52:22 +0700

    add header

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const styles = {
+    container:{
+        display: 'block',
+        margin: '0 auto',
+        width: '70%'
+    }
+};
+
+class Header extends React.Component{
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+export default Header

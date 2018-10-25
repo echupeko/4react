b45a64040151febdd0f266fdf00112814bacce0e
Евгений <echupeko@gmail.com>
2018-10-25 17:01:16 +0700

    updateasdasdas

--- src/App.js
+++ src/App.js
@@ -2,41 +2,25 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Body from './components/Body';
-import TodoItem from "./components/TodoItem";
+
 
 class App extends React.Component {
 
-    state = {
-        descriptionInput: 'Input your task',
-        todoList: []
-    };
 
     handleButton = () =>{
         this.setState({descriptionInput: this.changeDescriptionHandle});
         console.log('description = ', this.state.descriptionInput);
-        const list = this.state.todoList;
-        list.push({setName: 'todo Title', setUser: 'lalala'});
 
-        this.setState({todoList: list})
     };
 
     changeDescriptionHandle = (event) => {
         this.setState({descriptionInput: event.value})
     };
 
-    deleteItems = (index) =>{
-        console.log('delete: ', index)
-    }
 
-    titleChangeHandle = (index) => (event) => {
-        console.log('eevent2', event);
-        const list = this.state.todoList;
-        list[index].setName = event.target.value;
-        this.setState({todoList: list})
-    };
 
     render() {
-        const {todoList} = this.state;
+
         return (
             <div className="App">
                 <Header
@@ -45,14 +29,7 @@ class App extends React.Component {
                     changeDescriptionHandle={this.changeDescriptionHandle}
                     />
                 <Body/>
-                {todoList.map((element,index) =>
-                    <TodoItem
-                        titleChangeHandle={this.titleChangeHandle}
-                        setName={element.setName}
-                        deleteItem={this.deleteItems(index)}
-                        setUser={element.setUser}
-                        index={index}
-                        key={index}/>)}
+
             </div>
         );
     }
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,46 @@
 import React from 'react';
 import Form from './Form';
 import '../style/Body.css';
-
+import TodoItem from "./TodoItem";
 
 class Header extends React.Component{
 
+    state = {
+        descriptionInput: 'Input your task',
+        todoList: []
+    };
+
+
+    const list = this.state.todoList;
+    list.push({setName: 'todo Title', setUser: 'lalala'});
+
+    this.setState({todoList: list})
+
+    deleteItems = (index) =>{
+        console.log('delete: ', index)
+    }
+
+    titleChangeHandle = (index) => (event) => {
+        console.log('eevent2', event);
+        const list = this.state.todoList;
+        list[index].setName = event.target.value;
+        this.setState({todoList: list})
+    };
+
     render() {
+        const {todoList} = this.state;
         return (
             <div className="Body">
                 <Form/>
+                {todoList.map((element,index) =>
+                    <TodoItem
+                        titleChangeHandle={this.titleChangeHandle}
+                        setName={element.setName}
+                        deleteItem={this.deleteItems(index)}
+                        setUser={element.setUser}
+                        index={index}
+                        key={index}/>)}
+
             </div>
         )
     }
